28cd819cb7130d01ead6d638eb1f02f60a9bc099
Justyn Temme <Justyntemme@gmail.com>
2017-08-10 23:21:37 -0500

    added skeleton for Go-lang server

---
+++ server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/justyntemme/itsaRap/server/restAPI"
+)
+
+func main() {
+	fmt.Println("We work!")
+	restAPI.Go()
+
+}
---
+++ server/restAPI/restAPI.go
@@ -0,0 +1,13 @@
+package restAPI
+
+import (
+	"fmt"
+)
+
+//Run Starts Go REST API server
+func Run() {
+
+	go func() {
+		fmt.Print("goRoutine STarted!")
+	}()
+}
