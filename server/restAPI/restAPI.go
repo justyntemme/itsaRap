21fcc2855620f30a27d59e422b0ec897f197f302
Justyn Temme <Justyntemme@gmail.com>
2017-08-10 23:48:05 -0500

    added userStruct as well as skeleton for CreateUser

--- server/restAPI/restAPI.go
+++ server/restAPI/restAPI.go
@@ -2,12 +2,37 @@ package restAPI
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
+
+//.Data
+
+//User Struct holds all information about users"
+type User struct {
+	Name       string   `json:"user_Name"`
+	ID         string   `json:"user_ID"`
+	Pass       string   `json:"user_Pass"`
+	AvatarURL  string   `json:"avatar_URL"`
+	Songs      []string `json:"user_Songs"`
+	Comments   []string `json:"user_comments"`
+	Recordings []string `json:"user_recordings"`
+}
 
 //Run Starts Go REST API server
 func Run() {
 
 	go func() {
 		fmt.Print("goRoutine Started!")
+		r := httprouter.New()
+		r.POST("/user", createUser)
+		http.ListenAndServe("localhost:8080", r)
+
 	}()
+}
+
+func createUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	//json.NewDecoder(r.Body).Decode(//Right Here put user struct)
+
 }
