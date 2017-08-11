5a085d700188fa496307bc79a8733e78c7d43a19
Justyn Temme <Justyntemme@gmail.com>
2017-08-11 10:51:01 -0500

    Added mongodb integration for createUser

--- server/restAPI/restAPI.go
+++ server/restAPI/restAPI.go
@@ -7,24 +7,25 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 //.Data
 
 //User Struct holds all information about users"
 type User struct {
-	Name       string   `json:"user_Name"`
-	ID         string   `json:"user_ID"`
-	Pass       string   `json:"user_Pass"`
-	AvatarURL  string   `json:"avatar_URL"`
-	Songs      []string `json:"user_Songs"`
-	Comments   []string `json:"user_comments"`
-	Recordings []string `json:"user_recordings"`
+	Name       string        `json:"user_Name" bson:"user_name"`
+	Id         bson.ObjectId `json:"user_ID" bson:"User_ID"`
+	Pass       string        `json:"user_Pass" bson:"user_Pass"`
+	AvatarURL  string        `json:"avatar_URL" bson:"avatar_URL"`
+	Songs      []string      `json:"user_Songs" bson:"user_Songs"`
+	Comments   []string      `json:"user_comments" bson:"user_Comments"`
+	Recordings []string      `json:"user_recordings" bson:"user_Recordings"`
 }
 
 //UserController holds the session value
 type UserController struct {
-	sssion *mgo.Session
+	session *mgo.Session
 }
 
 //Run Starts Go REST API server
@@ -32,19 +33,47 @@ func Run() {
 
 	go func() {
 		fmt.Print("goRoutine Started!")
+		uc := NewUserController(getSession())
 		r := httprouter.New()
-		r.POST("/user", createUser)
+		r.POST("/user", uc.CreateUser)
 		http.ListenAndServe("localhost:8080", r)
 
 	}()
+
 }
 
-func createUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	u := new(User)
+//CreateUser creates a user entry in the mongoDB
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := User{}
+
 	json.NewDecoder(r.Body).Decode(&u)
 
+	//Create bson ID
+	u.Id = bson.NewObjectId()
+
+	//store the user in mongodb
+	uc.session.DB("its-a-rap-db").C("users").Insert(u)
+
+	uj, _ := json.Marshal(u)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) //201
+	fmt.Fprintf(w, "%s\n", uj)
+
 }
 
+//NewUserController creates a new mongo UserController with session embedded
 func NewUserController(s *mgo.Session) *UserController {
 	return &UserController{s}
+}
+
+//getSessino connects to mongodb and returns session ID
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost")
+
+	//Check if connection err, is mongo running?
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
