c46f6c4e192c5ba982fe26f1780d1dbafa3c8b83
Justyn Temme <Justyntemme@gmail.com>
2017-08-10 22:39:02 -0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'app';
+  public loggedIn:boolean = false;
+
+
+
+    ngOnInit() {
+      
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { LoggedInComponent } from './logged-in/logged-in.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    LoggedInComponent,
+    NotificationsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
